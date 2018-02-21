7fb0ea24191f1c6633b8ddb08c9284d3076c46ea
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-02-21 08:14:04 +0100

    Ajout header-full-img-cta-video

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -26,6 +26,7 @@ AOS.init();
     function toggleNav() {
 
 // Define targets by their class or id
+        var burger = document.querySelector('#burger');
         var button = document.querySelector('.nav-button');
         var content = document.querySelector('.content');
         var target = document.querySelector('header');
@@ -35,6 +36,7 @@ AOS.init();
         if ( button ) {
             button.addEventListener('click',
                 function (e) {
+                    burger.classList.toggle('is-active');
                     button.classList.toggle('is-active');
                     content.classList.toggle('is-hidden');
                     target.classList.toggle('is-opened');
@@ -46,7 +48,7 @@ AOS.init();
 
     toggleNav();
 
-    function  burger() {
+    function burger() {
         var trigger = $('#hamburger'),
             isClosed = true;
 
@@ -75,4 +77,63 @@ $(function () {
         e.preventDefault();
         $('html, body').animate({'scrollTop': 0}, 400);
     });
+});
+
+$(function () {
+    $('.js-scroll-down').click(function (e) {
+        var $this = $(this);
+
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $this.closest('.js-block').next().offset().top,
+        }, 600);
+
+        $this.trigger('blur');
+    });
+});
+
+$(function () {
+    "use strict";
+
+    var $activateButton = $('.buttons__watch');
+    var $videoPopup = $('.video__popup');
+    var $closeButton = $('.video__popup__close');
+
+    var videoFrame = document.getElementById('video-frame');
+
+    if (window.Vimeo) {
+        var player = new Vimeo.Player(videoFrame);
+    }
+
+
+    $activateButton.on('click', function (event) {
+        event.preventDefault();
+        $videoPopup.toggleClass('is-hidden');
+    });
+
+    $closeButton.on('click', function () {
+        $videoPopup.toggleClass('is-hidden');
+        player.unload();
+    });
+
+    if ($(window).width() <= 1020) {
+        $(videoFrame).attr({
+            width: 500,
+            height: 281
+        });
+    }
+
+    if ($(window).width() <= 520) {
+        $(videoFrame).attr({
+            width: 320,
+            height: 180
+        });
+    }
+
+    /*if ($(window).width() <= 750) {
+        $('.header-5__logo').prependTo('.header-5__middle');
+        $('.header-5__tag').remove();
+    }*/
+
 });
