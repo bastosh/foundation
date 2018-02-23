84074081b01ae67d1a85796d6b1b0912c5e8ce70
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-02-23 22:40:30 +0100

    Ajout template service, travail sur les footers

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -217,3 +217,59 @@ $(function () {
             })
     });
 });
+
+$(function () {
+    "use strict";
+
+
+    var $select = $('.subscribe__form__select');
+
+
+    $select.each(function () {
+        var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+        $this.addClass('is-hidden');
+        $this.wrap('<div class="select"></div>');
+        $this.after('<div class="select-styled"></div>');
+
+        var $styledSelect = $this.next('div.select-styled');
+
+
+        var $list = $('<ul />', {
+            'class': 'select-options'
+        }).insertAfter($styledSelect);
+
+        for (var i = 0; i < numberOfOptions; i++) {
+            $('<li />', {
+                rel: $this.children('option').eq(i).val()
+            }).prepend($('.select__icons').find('[data-country="' + $this.children('option').eq(i).data('country') + '"]').clone())
+                .append('<div class="select__icons__text"><div class="select__icons__text__number">' + $.trim($this.children('option').eq(i).html()) + '</div><div class="select__icons__text__country">' + $this.children('option').eq(i).data('country-full') + '</div></div>').appendTo($list);
+        }
+
+        $styledSelect.append('<div class="select__icons__text">' + $.trim($this.children('option').eq(0).html()) + '</div>')
+            .prepend($('.select__icons').find('[data-country="' + $this.children('option').eq(0).data('country') + '"]').clone());
+
+        var $listItems = $list.children('li');
+
+        $styledSelect.click(function (e) {
+            e.stopPropagation();
+            $('div.select-styled.active').not(this).each(function () {
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+
+        $listItems.click(function (e) {
+            e.stopPropagation();
+            $styledSelect.html($(this).html()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+        });
+
+        $(document).click(function () {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
+
+    });
+});
